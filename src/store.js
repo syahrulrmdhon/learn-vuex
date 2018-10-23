ae29854824a9c913f33e30ffecd43b76f1e31a44
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-10-23 13:07:02 +0700

    add and remove component

--- src/store.js
+++ src/store.js
@@ -5,12 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    title: 'Custom titile',
+    links: [
+      'https://google.com',
+      'https://youtube.com',
+      'https://linkedin.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {        // Add this:
+      state.links.splice(link, 1)
+    }
   },
   actions: {
-
+    removeLink: (context, link) => {       // Add this:
+      context.commit("REMOVE_LINK", link)
+    }
   }
 })
